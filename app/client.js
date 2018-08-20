2bc2742f6128e6ce81566f06087fcaa7853160a7
dmistera2 <dmistera2@gmail.com>
2018-08-21 01:47:53 +0200

    Fixed socket

--- app/client.js
+++ app/client.js
@@ -1,18 +1,22 @@
 "use strict";
 var url = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/ws/";
-var socket = new WebSocket(url);
-socket.onclose = function () {
-    console.log("Socket reopened");
-    socket = new WebSocket(url);
-};
+var socket;
 var textfield = document.getElementById("textbox");
-socket.onopen = function (e) {
-    socket.onmessage = function (messageEvent) {
-        var line = document.createElement("p");
-        line.innerHTML = messageEvent.data;
-        textfield.appendChild(line);
+function setSocket() {
+    socket = new WebSocket(url);
+    socket.onclose = function () {
+        console.log("Socket reopened");
+        setSocket();
     };
-};
+    socket.onopen = function (e) {
+        socket.onmessage = function (messageEvent) {
+            var line = document.createElement("p");
+            line.innerHTML = messageEvent.data;
+            textfield.appendChild(line);
+        };
+    };
+}
+setSocket();
 function sendMessage() {
     var username = document.getElementById("username").value;
     var messageField = document.getElementById("message");
--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,25 @@
 
 var url = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/ws/";
-var socket = new WebSocket(url);
-socket.onclose = () => {
-    console.log("Socket reopened");
-    socket = new WebSocket(url);
-}
+var socket : WebSocket;
 var textfield = document.getElementById("textbox") as HTMLElement;
 
-socket.onopen = (e) => {
-    socket.onmessage = (messageEvent) => {
-        var line = document.createElement("p");
-        line.innerHTML = messageEvent.data;
-        textfield.appendChild(line);
+function setSocket() {
+    socket = new WebSocket(url);
+    socket.onclose = () => {
+        console.log("Socket reopened");
+        setSocket();
     }
+    socket.onopen = (e) => {
+        socket.onmessage = (messageEvent) => {
+            var line = document.createElement("p");
+            line.innerHTML = messageEvent.data;
+            textfield.appendChild(line);
+        }
+    }
+    
 }
+
+setSocket();
 
 function sendMessage() {
     var username = (document.getElementById("username") as any).value;
